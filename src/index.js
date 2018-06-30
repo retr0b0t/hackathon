89d8cabdfc51af0e94fe4943660084ab783d5271
bnair2001 <36124624+bnair2001@users.noreply.github.com>
2018-06-30 09:14:31 +0530

    Add files via upload
    version 1.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr'
 import 'semantic-ui-css/semantic.min.css';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+import './index.css';
+import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <ReduxToastr
+            position='bottom-right'
+            transitionIn='fadeIn'
+            transitionOut='fadeOut'
+          />
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+store.firebaseAuthIsReady.then(() => {
+  render();
+  registerServiceWorker();
+})
+
